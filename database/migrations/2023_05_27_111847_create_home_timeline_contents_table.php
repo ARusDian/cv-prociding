56516fde1c12c975eae0fca068e897a1a3862b03
dimpram101 <dimas2004211@gmail.com>
2023-06-02 09:43:57 +0800

    fix: fix migration table name

--- database/migrations/2023_05_27_111847_create_home_timeline_contents_table.php
+++ database/migrations/2023_05_27_111847_create_home_timeline_contents_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('home_poster_contents', function (Blueprint $table) {
+        Schema::create('home_timeline_contents', function (Blueprint $table) {
             $table->id();
             $table->string('img_path');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('home_poster_contents');
+        Schema::dropIfExists('home_timeline_contents');
     }
 };
