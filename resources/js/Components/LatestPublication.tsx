ee9e48e599e2dba6fb34882e0774318fb6a09ccc
dimpram101 <dimas2004211@gmail.com>
2023-06-02 18:52:59 +0800

    fix: fixing layout

--- resources/js/Components/LatestPublication.tsx
+++ resources/js/Components/LatestPublication.tsx
@@ -11,10 +11,10 @@ const LatestPublication = ({ imgSrc, linkTo }: Props) => {
   return (
     <>
       <div className="w-[50%] flex flex-col items-center gap-4">
-        <div>
+        <div className='w-[70%] h-full'>
           <img src={imgSrc} alt="" className='object-contain w-full h-[700px] rounded-lg' />
         </div>
-        <a href={linkTo} target='_blank' className='bg-[#FFA500] px-4 py-2 text-lg font-black rounded-full hover:opacity-90 transition-all ease-out duration-300'>Access to Conference Paper Proceeding</a>
+        <a href={linkTo} target='_blank' className='bg-[#FFA500] px-4 py-2 text-xl font-black rounded-full hover:opacity-90 transition-all ease-out duration-300'>Access to Conference Paper Proceeding</a>
       </div>
     </>
   )
